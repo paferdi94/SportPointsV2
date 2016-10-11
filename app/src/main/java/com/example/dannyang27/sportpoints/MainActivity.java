1534b1b95466e83d6d67c09ab61eaa97c0dd5dbc
git <danny27995@gmail.com>
2016-10-11 15:12:18 +0200

    fdfsfdsf

--- app/src/main/java/com/example/dannyang27/sportpoints/MainActivity.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //comentario de prueba
+    //2 comentario
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
