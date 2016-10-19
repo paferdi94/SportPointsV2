0a5db1a107fdb688a78b276ee9d5b5fa0d7a47be
Pablo Fernandez Diaz <paferdi9@gmail.com>
2016-10-19 10:40:33 +0200

    prueba

--- app/src/main/java/com/example/dannyang27/sportpoints/activities/alta.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/alta.java
@@ -12,7 +12,7 @@ public class alta extends AppCompatActivity {
 
     private Button atras;
     private Button siguiente;
-
+//prueba1commit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
