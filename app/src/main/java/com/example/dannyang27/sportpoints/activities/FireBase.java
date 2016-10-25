a04d727c1bf260be116c3185711cc154d050b651
Pablo Fernandez Diaz <paferdi9@gmail.com>
2016-10-25 14:19:34 +0200

    -Sin carpetas -Nuevo:           -Equipo.java           -FireBase.java(no hacer caso son pruebas)

---
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/FireBase.java
@@ -0,0 +1,38 @@
+package com.example.dannyang27.sportpoints.activities;
+
+import android.os.Bundle;
+
+/**
+ * Created by paferdi94 on 23/10/2016.
+ * Manipulador Base de datos
+ */
+
+public class FireBase {
+
+    private  static final String FIREBASE_URL = "https://sport-points-7f5e0.firebaseio.com/"; // direccion de la BD
+    //private String FIREBASE_TABLA = ""; // hay que insertar el nombre de la tabla con la que queramos trabajar;
+/*
+    public String getFIREBASE_URL() {
+        return FIREBASE_URL;
+    }
+
+    public String getFIREBASE_TABLA() {
+        return FIREBASE_TABLA;
+    }
+    //constructor
+    public FireBase(String link, String tabla){
+        link = FIREBASE_URL;
+        this.FIREBASE_TABLA = tabla;
+
+    }
+
+    public devolucionDatos(){
+
+    }
+
+    protected void onCreate(Bundle saveInstanceSatet){
+        super.onCreate(saveInstanceSatet);
+
+
+    }*/
+}
