262ca62a523a2b04c48b898f224a9c22492b70d4
git <danny27995@gmail.com>
2016-11-15 00:27:16 +0100

    Version 5.1 RecyclerView Con Material Design en Participantes ------------------------ Border en las CardView + Background color priamary

--- app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EventoInfo_MD.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EventoInfo_MD.java
@@ -38,6 +38,16 @@ public class EventoInfo_MD extends AppCompatActivity {
         descripcion_evento_info = (TextView) findViewById(R.id.descripcion_evento_info_md);
 
         evento_info_img.setImageResource(R.drawable.mestalla2);
+        nombre_evento_info.setText("VALENCIA C.F vs LEVANTE C.D");
+        lugar_evento_info.setText("Mestalla");
+        fecha_evento_info.setText("Miercoles, Septiembre 2017");
+        hora_evento_info.setText("20:00h");
+        descripcion_evento_info.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad " +
+                "minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
+                " cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non" +
+                " proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
 
         btn_evento_info.setOnClickListener(new View.OnClickListener() {
