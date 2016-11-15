2ba54d3c11009b8b519913462fab02d965ed3f98
git <danny27995@gmail.com>
2016-11-16 00:49:05 +0100

    Version 5.3 LogIn + Registro + EquipoInfo_Material Design

---
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EquipoInfo_MD.java
@@ -0,0 +1,54 @@
+package com.example.dannyang27.sportpoints.activities.PruebasDanny;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dannyang27.sportpoints.R;
+
+import org.w3c.dom.Text;
+
+
+public class EquipoInfo_MD extends AppCompatActivity {
+
+    private ImageView imagenEquipo;
+    private TextView nombreTxt;
+    private TextView deporteTxt;
+    private TextView participantesTxt;
+    private Button verPartBtn;
+    private Button unirseBtn;
+    private TextView descripcionTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equipo_info__md);
+
+        nombreTxt = (TextView) findViewById(R.id.nombre_equipo_info_md);
+        imagenEquipo = (ImageView) findViewById(R.id.image_equipo_info_md);
+        deporteTxt = (TextView) findViewById(R.id.deporte_equipo_info_md);
+        participantesTxt = (TextView) findViewById(R.id.participantes_equipo_info_md);
+        verPartBtn = (Button) findViewById(R.id.verParticipantes_equipo_info_md);
+        unirseBtn = (Button) findViewById(R.id.unirse_equipo_info_md);
+        descripcionTxt = (TextView) findViewById(R.id.descripcion_equipo_info_md);
+
+
+        imagenEquipo.setImageResource(R.drawable.denia_logo);
+        nombreTxt.setText("C.D.Denia");
+        deporteTxt.setText("Futbol");
+        participantesTxt.setText("0/11");
+        descripcionTxt.setText("At vero eos et accusamus et iusto odio dignissimos ducimus qui" +
+                " blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas" +
+                " molestias excepturi sint occaecati cupiditate non provident, similique sunt in" +
+                " culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et" +
+                " harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum" +
+                " soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime " +
+                "placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. " +
+                "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe" +
+                " eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque " +
+                "earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus" +
+                " maiores alias consequatur aut perferendis doloribus asperiores repellat.");
+    }
+}
