9b7e5c6db5cc60292881e6ef255f2fbd86bf01e0
Dannyang27 <danny27995@gmail.com>
2016-11-15 14:25:27 +0100

    Version 5.2
    Clases Parceables y participantes redisenyado

--- app/src/main/java/com/example/dannyang27/sportpoints/activities/Modelos/Participante.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/Modelos/Participante.java
@@ -5,15 +5,45 @@ package com.example.dannyang27.sportpoints.activities.Modelos;
  */
 
 public class Participante {
+    private String dni;
     private String nombre;
+    private String fecha;
+    private String email;
     private String telefono;
 
     public Participante() {
     }
 
-    public Participante(String nombre, String telefono) {
-        this.nombre = nombre;
+    public Participante(String dni, String email, String fecha, String telefono, String nombre) {
+        this.dni = dni;
+        this.email = email;
+        this.fecha = fecha;
         this.telefono = telefono;
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
     }
 
     public String getNombre() {
@@ -31,4 +61,5 @@ public class Participante {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
 }
