e591ef5bf8ef7b54ebe56a5cb9ce6eef9888eb1d
mikel240 <miguel_real_2@hotmail.com>
2016-11-25 13:39:56 +0100

    darse de baja evento

--- app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EventoInfo_MD.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EventoInfo_MD.java
@@ -1,10 +1,11 @@
 package com.example.dannyang27.sportpoints.activities.PruebasDanny;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +16,6 @@ import android.widget.Toast;
 
 import com.example.dannyang27.sportpoints.R;
 import com.example.dannyang27.sportpoints.activities.Modelos.EventoPruebaDanny;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -25,13 +25,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class EventoInfo_MD extends AppCompatActivity {
 
+    Boolean usuarioEncontrado = false;
+    private View rootView;
     private ImageView evento_info_img;
+    private TextView unirse_a_evento;
     private TextView nombre_evento_info;
     private TextView lugar_evento_info;
     private TextView fecha_evento_info;
@@ -41,10 +43,11 @@ public class EventoInfo_MD extends AppCompatActivity {
     private TextView descripcion_evento_info;
 
     FirebaseStorage firebaseStorageRef = FirebaseStorage.getInstance();
-    StorageReference mStorageRef =firebaseStorageRef.getReference();
+    StorageReference mStorageRef = firebaseStorageRef.getReference();
 
     FirebaseDatabase mDataRef = FirebaseDatabase.getInstance();
     DatabaseReference participantesRef = mDataRef.getReference();
+    String participanteKey;
 
     private ArrayList<String> listaParticipantes = new ArrayList<>();
 
@@ -53,6 +56,7 @@ public class EventoInfo_MD extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento_info__md);
 
+        rootView = (View) findViewById(R.id.activity_evento_info__md);
         unirse_btn = (Button) findViewById(R.id.unirse_evento_md);
         evento_info_img = (ImageView) findViewById(R.id.evento_info_img);
         nombre_evento_info = (TextView) findViewById(R.id.nombre_evento_info_md);
@@ -61,19 +65,27 @@ public class EventoInfo_MD extends AppCompatActivity {
         hora_evento_info = (TextView) findViewById(R.id.hora_evento_info_md);
         btn_evento_info = (Button) findViewById(R.id.btn_evento_info_md);
         descripcion_evento_info = (TextView) findViewById(R.id.descripcion_evento_info_md);
+        unirse_a_evento = (TextView) findViewById(R.id.unirse_evento_md);
 
         EventoPruebaDanny e = getIntent().getParcelableExtra("PARCELABLE");
 
         //vamos a la ruta de la imagen
 
-        mStorageRef = mStorageRef.child("eventos/"+e.getImagen());
+        mStorageRef = mStorageRef.child("eventos/" + e.getImagen());
+
         participantesRef = participantesRef.child("Eventos")
                 .child(e.getNombre().toString()).child("participantes");
+
 
         participantesRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 String str = dataSnapshot.getValue(String.class).toString();
+                if (!usuarioEncontrado && "rita".equals(str)) {
+                    unirse_a_evento.setText("No voy a ir");
+                    usuarioEncontrado = true;
+                    participanteKey = dataSnapshot.getKey();
+                }
                 listaParticipantes.add(str);
 
             }
@@ -85,7 +97,8 @@ public class EventoInfo_MD extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-
+                String usuarioBorrado = dataSnapshot.getValue(String.class).toString();
+                borrarElementoLista(usuarioBorrado);
             }
 
             @Override
@@ -98,7 +111,6 @@ public class EventoInfo_MD extends AppCompatActivity {
 
             }
         });
-
 
 
         nombre_evento_info.setText(e.getNombre().toString());
@@ -117,13 +129,12 @@ public class EventoInfo_MD extends AppCompatActivity {
         });
 
 
-
         btn_evento_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              // showParticipantesInfo();
-               // Snackbar.make(view,getListaParticipantes(), Snackbar.LENGTH_LONG).show();
-                Toast.makeText(getApplicationContext(),getListaParticipantes(),Toast.LENGTH_LONG).show();
+                // showParticipantesInfo();
+                // Snackbar.make(view,getListaParticipantes(), Snackbar.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), getListaParticipantes(), Toast.LENGTH_LONG).show();
 
             }
         });
@@ -131,10 +142,34 @@ public class EventoInfo_MD extends AppCompatActivity {
         unirse_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view,"Te has unido al evento correctamente", Snackbar.LENGTH_LONG).show();
+                if (unirse_a_evento.getText().equals("No voy a ir")) {
+                    new AlertDialog.Builder(view.getContext())
+                            .setTitle("Cancelar participación")
+                            .setMessage("Estás seguro que deseas cancelar tu asistencia al evento?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    participantesRef.child(participanteKey).removeValue();
+                                    usuarioEncontrado = false;
+                                    unirse_a_evento.setText("Unirse");
+                                    Snackbar.make(rootView, "Has cancelado tu asistencia correctamente", Snackbar.LENGTH_LONG).show();
+
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            })
+                            .show();
+                } else {
+                    //implementar en la BDD - unir usuario a evento
+                    Snackbar.make(view, "Te has unido al evento correctamente", Snackbar.LENGTH_LONG).show();
+                }
             }
         });
+    }
 
+    private void mostrarSnackBar() {
 
     }
 
@@ -143,14 +178,19 @@ public class EventoInfo_MD extends AppCompatActivity {
         startActivity(i);
     }
 
-    private String getListaParticipantes(){
+    private String getListaParticipantes() {
         String aux = "";
-        for(int i=0; i < listaParticipantes.size();i++){
-            aux += listaParticipantes.get(i)+"\n";
-
+        for (int i = 0; i < listaParticipantes.size(); i++) {
+            aux += listaParticipantes.get(i) + "\n";
         }
         return aux;
     }
 
+    private void borrarElementoLista(String usuario){
+        for(int i = 0; i < listaParticipantes.size();i++){
+            if(usuario.equals(listaParticipantes.get(i)))
+                listaParticipantes.remove(i);
+        }
+    }
 
 }
