8ebb3274fa74b3ee514843a0f93979647bd0ee55
mikel240 <miguel_real_2@hotmail.com>
2016-11-29 01:37:03 +0100

    eve

--- app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EventoFragmento.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/PruebasDanny/EventoFragmento.java
@@ -324,20 +324,6 @@ public class EventoFragmento extends Fragment {
     }
 
 
-    //Comprobar si tenemos internet en un momento determinado
-    public Boolean isOnlineNet() {
 
-        try {
-            Process p = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.es");
-            int val = p.waitFor();
-            boolean reachable = (val == 0);
-            return reachable;
-
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return false;
-    }
 
 }
