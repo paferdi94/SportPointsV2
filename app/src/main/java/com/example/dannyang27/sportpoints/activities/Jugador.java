37398864b79bce874ea0c3acdc86b5f4f80b2101
mikel240 <miguel_real_2@hotmail.com>
2016-12-13 19:31:25 +0100

    calificar jugador

--- app/src/main/java/com/example/dannyang27/sportpoints/activities/Jugador.java
+++ app/src/main/java/com/example/dannyang27/sportpoints/activities/Jugador.java
@@ -18,10 +18,15 @@ public class Jugador implements Parcelable {
     private String nombre;
     private String apellidos;
     private String email;
-    private int telefono;
-    private Date fechaNacimiento;
+    private long telefono;
+    private String fechaNacimiento;
+    double valoracion;
+    int numValoraciones;
 
-    public Jugador(String login, String password, String nombre, String apellidos, String email, int telefono, Date fechaNacimiento){
+    public Jugador() {
+    }
+
+    public Jugador(String apellidos, String email, String fechaNacimiento, String login, String nombre, int numValoraciones, String password, long telefono, double valoracion) {
         this.login = login;
         this.password = password;
         this.nombre = nombre;
@@ -29,62 +34,97 @@ public class Jugador implements Parcelable {
         this.email = email;
         this.telefono = telefono;
         this.fechaNacimiento = fechaNacimiento;
+        this.valoracion = valoracion;
+        this.numValoraciones = numValoraciones;
     }
 
-    public String getLogin(){
+    public String getLogin() {
         return this.login;
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return this.password;
     }
-    public String getNombre(){
+
+    public String getNombre() {
         return this.nombre;
     }
-    public String getApellidos(){
+
+    public String getApellidos() {
         return this.apellidos;
     }
-    public String getEmail(){
+
+    public String getEmail() {
         return this.email;
     }
-    public int getTelefono(){
+
+    public long getTelefono() {
         return this.telefono;
     }
-    public Date getFechaNacimiento(){
+
+    public String getFechaNacimiento() {
         return this.fechaNacimiento;
     }
 
-    public void setLogin(String login){
+    public int getNumValoraciones() {
+        return numValoraciones;
+    }
+
+    public double getValoracion() {
+        return valoracion;
+    }
+
+
+    public void setLogin(String login) {
         this.login = login;
     }
-    public void setPassword(String password){
+
+    public void setPassword(String password) {
         this.password = password;
     }
-    public void setNombre(String nombre){
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public void setApellidos(String apellidos){
+
+    public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
-    public void setEmail(String email){
+
+    public void setEmail(String email) {
         this.email = email;
     }
-    public void setTelefono(int telefono){
+
+    public void setTelefono(long telefono) {
         this.telefono = telefono;
     }
-    public void setFechaNacimiento(Date fechaNacimiento){
+
+    public void setFechaNacimiento(String fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
 
-    public Map<String, Object> toMap() {
+    public void setValoracion(double valoracion) {
+        this.valoracion = valoracion;
+    }
 
+    public void setNumValoraciones(int numValoraciones) {
+        this.numValoraciones = numValoraciones;
+    }
+
+
+    public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("login", login);
-        result.put("password", password);
-        result.put("nombre", nombre);
         result.put("apellidos", apellidos);
         result.put("email", email);
+        result.put("fechaNacimiento", fechaNacimiento);
+        result.put("login", login);
+        result.put("nombre", nombre);
+        result.put("numValoraciones", numValoraciones);
+        result.put("password", password);
         result.put("telefono", telefono);
-        result.put("fechaNacimiento", (new SimpleDateFormat("dd/MM/yyyy")).format(fechaNacimiento));
+        result.put("valoracion", valoracion);
+        //result.put("fechaNacimiento", (new SimpleDateFormat("dd/MM/yyyy")).format(fechaNacimiento));
+        //result.put("valoracion", valoracion);
         return result;
     }
 
@@ -97,13 +137,17 @@ public class Jugador implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(login);
-        parcel.writeString(password);
-        parcel.writeString(nombre);
         parcel.writeString(apellidos);
         parcel.writeString(email);
-        parcel.writeInt(telefono);
-        parcel.writeLong(fechaNacimiento.getTime());
+        parcel.writeString(fechaNacimiento);
+        parcel.writeString(login);
+        parcel.writeString(nombre);
+        parcel.writeInt(numValoraciones);
+        parcel.writeString(password);
+        parcel.writeLong(telefono);
+        parcel.writeDouble(valoracion);
+        //parcel.writeLong(fechaNacimiento.getTime());
+        // parcel.writeInt(valoracion);
     }
 
     public static final Parcelable.Creator<Jugador> CREATOR = new Parcelable.Creator<Jugador>() {
@@ -117,12 +161,16 @@ public class Jugador implements Parcelable {
     };
 
     private Jugador(Parcel in) {
-        login = in.readString();
-        password = in.readString();
-        nombre = in.readString();
         apellidos = in.readString();
         email = in.readString();
-        telefono = in.readInt();
-        fechaNacimiento = new Date(in.readLong());
+        fechaNacimiento = in.readString();
+        login = in.readString();
+        nombre = in.readString();
+        numValoraciones = in.readInt();
+        password = in.readString();
+        telefono = in.readLong();
+        valoracion = in.readDouble();
+        //fechaNacimiento = new Date(in.readLong());
+        // valoracion= in.readInt();
     }
 }
