33298afaa533cc854a4204d783ad993d685198f1
paferdi94 <paferdi9@gmail.com>
2017-05-12 21:41:52 +0200

    Creacion Factory Method

---
+++ app/src/main/java/com/dannyang27/sportpoints/activities/Fabricas/FabricaJugadores.java
@@ -0,0 +1,17 @@
+package com.dannyang27.sportpoints.activities.Fabricas;
+
+/**
+ * Created by PAFER on 12/05/2017.
+ */
+
+import com.dannyang27.sportpoints.activities.Interfaces.Usuario;
+import com.dannyang27.sportpoints.activities.Jugador;
+
+public class FabricaJugadores implements Usuario{
+
+    public Usuario crearJugador(){
+        Jugador j = new Jugador();
+        j.
+    }
+
+}
